c962b5a33b40c26adc9804f776c3869701e10c01
alexis24 <Akihiro_18>
2017-09-02 12:39:36 -0500

    Initial Commit

---
+++ HackatonReniecIOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HackatonReniecIOS
+//
+//  Created by Alexis Leon Shimabukuro on 2/09/17.
+//  Copyright © 2017 DAIF. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
